6a444e3899d470c22886f1e602b4e4d716596b38
Robert <rlgallegos85@gmail.com>
2023-12-28 00:03:39 -0500

    finished johnn's program

--- playground.py
+++ playground.py
@@ -1,34 +1,71 @@
 from docx import Document
 
-efforts = [
-    'Direct Space',
-    'Indirect Space',
-    'Strong Weight',
-    'Light Weight',
-    'Sudden Time',
-    'Sustained Time',
-    'Bound Flow',
-    'Free Flow'
-]
-
-actions = [
-    'Float',
-    'Punch',
-    'Glide',
-    'Slash',
-    'Dab',
-    'Wring',
-    'Flick',
-    'Press'
+# main_efforts = [
+#     'Direct',
+#     'Indirect',
+#     'Strong',
+#     'Light',
+#     'Sudden',
+#     'Sustained',
+#     'Bound',
+#     'Free'
+# ]
+effort_subs = [
+    [
+        'Direct',
+        'Indirect'
+    ],
+    [
+        'Strong',
+        'Light'
+    ],
+    [
+        'Sudden',
+        'Sustained'
+    ],
+    [
+        'Bound',
+        'Free'
+    ]
 ]
 
 result = []
+for space in effort_subs[0]:
+    for weight in effort_subs[1]:
+        for time in effort_subs[2]:
+            for flow in effort_subs[3]:
+                combo = space + ' - ' + weight + ' - ' + time + ' - ' + flow
+                result.append(combo)
 
-for effort in efforts:
-    result.append('///')
-    for action in actions:
-        combo = effort + ' - ' + action
-        result.append(combo)
+# efforts = [
+#     'Direct Space',
+#     'Indirect Space',
+#     'Strong Weight',
+#     'Light Weight',
+#     'Sudden Time',
+#     'Sustained Time',
+#     'Bound Flow',
+#     'Free Flow'
+# ]
+
+# actions = [
+#     'Float',
+#     'Punch',
+#     'Glide',
+#     'Slash',
+#     'Dab',
+#     'Wring',
+#     'Flick',
+#     'Press'
+# ]
+
+
+
+# for effort in efforts:
+#     result.append('///')
+#     for action in actions:
+#         combo = effort + ' - ' + action
+#         result.append(combo)
 
 for r in result:
     print()
@@ -41,14 +78,45 @@ document.add_heading("Johnny's List", level=1)
 
 # Create a paragraph and add the list items as bullet points
 i = 0
-for item in result:
-    if item == '///':
-        document.add_heading(efforts[i // 8])
+
+for i in range(len(result)):
+    if i % 8 == 0:
         paragraph = document.add_paragraph()
-    elif i % 2 == 0:
-        paragraph.add_run(item + "\u00a0\u00a0\u00a0").font.name = "Times New Roman"
-    else:
-        paragraph.add_run(item + "\n").font.name = "Times New Roman"
-    i += 1
+    paragraph.add_run(result[i] + "\n").font.name = "Times New Roman"
+    
+
+# Over each main effort
+# for i in range(4):
+#     effort = main_efforts[i]
+#     document.add_heading(effort, level=1)
+
+#     # Over each sub effort:
+#     for j in range(2):
+#         sub_effort = effort_subs[i][j]
+#         document.add_heading(sub_effort, level=2)
+#         paragraph = document.add_paragraph()
+
+#         # Over each action
+#         for k in range(8):
+#             action = actions[k]
+#             paragraph.add_run(action + "\u00a0\u00a0\u00a0").font.name = "Times New Roman"
+
+
+
+
+
+
+# for item in result:
+#     if i % 16 == 0 and i != 64:
+#         print(i)
+#         document.add_heading(main_efforts[i // 16])
+
+#     if item == '///':
+#         paragraph = document.add_paragraph()
+#     elif i % 2 == 0:
+#         paragraph.add_run(item + "\u00a0\u00a0\u00a0").font.name = "Times New Roman"
+#     else:
+#         paragraph.add_run(item + "\n").font.name = "Times New Roman"
+#     i += 1
 
 document.save("johnny-list.docx")
